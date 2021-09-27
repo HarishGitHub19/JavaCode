326946210d57e87d9fa2bf2c274fbd9fd0679416
HarishGitHub19 <91373956+HarishGitHub19@users.noreply.github.com>
2021-09-27 13:54:19 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,17 @@
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		int a = 40,b = 500 , c = 100;
+		if(a > b && a > c) {
+			System.out.println(a);
+		}
+		else if(b > a && b > c) {
+			System.out.println(b);
+		}
+		else {
+			System.out.println(c);
+		}
+
+	}
+
+}
---
+++ Sumofnumbers.java
@@ -0,0 +1,12 @@
+public class Sumofnumber {
+
+	public static void main(String[] args) {
+		int num = 0;
+		for(int i = 1;i<=100;i++) {
+			num = num+i;
+		}
+		System.out.println(num);
+
+	}
+
+}
